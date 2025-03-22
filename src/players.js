24990431920d94fca59da5ff308b8ee7f74256cf
mohamed <amalkirifi01@gmail.com>
2025-03-22 02:49:07 +0000

    adding my project

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://fifpro.org/media/fhmfhvkx/messi-world-cup.jpg?rxy=0.48356841796117644,0.31512414378031967&width=1000&height=640&rnd=133210253587130000"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://media.gettyimages.com/id/2178130283/fr/photo/warsaw-poland-cristiano-ronaldo-reacts-after-scoring-his-team-second-goal-during-the-uefa.jpg?s=1024x1024&w=gi&k=20&c=QalEMNksSgvMK9BBhq07bKbm_OcqEQ7rZsBDxCBBz6M="
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://cdn.britannica.com/39/239139-050-49A950D1/French-soccer-player-Kylian-Mbappe-FIFA-World-Cup-December-10-2022.jpg"
+    },
+    {
+      name: "Kevin De Bruyne",
+      team: "Manchester City",
+      nationality: "Belgium",
+      jerseyNumber: 17,
+      age: 32,
+      imageUrl: "https://s.hs-data.com/bilder/spieler/gross/142263.jpg?fallback=png"
+    }
+  ];
+  
+  export default players;
+  
